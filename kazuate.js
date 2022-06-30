ca94fe7993a3fa88bd49f3c66c09d884e57377ee
G184822021 <218482@st.takushoku-u.ac.jp>
2022-06-30 15:36:16 +0900

    2022/06/30　変更、追加、修正

--- kazuate.js
+++ kazuate.js
@@ -7,23 +7,28 @@ hantei();
 hantei();
 hantei();
 hantei();
-hantei();
-hantei();
 function hantei(){
     let yoso = 4;
     answerCount = answerCount + 1;
-    console.log(answerCount + '回目の予想: ' + yoso);
+    let print;
+    let answerNode = document.querySelector('span#answerCount');
+    answerNode.textContent = answerCount;
+    answerNode = document.querySelector('span#yoso');
+    answerNode.textContent = yoso;
     if(answerCount > 3) {
-        console.log('答えは ' + answer + ' でした.すでにゲームは終わっています.');
-        return;
-    }
-    if(yoso === answer){
-        console.log('正解です.おめでとう！');
+        print = '答えは ' + answer + ' でした.すでにゲームは終わっています.';
+    } else if(yoso === answer){
+        print = '正解です.おめでとう！';
     } else if(answerCount === 3){
-        console.log('間違い.残念でした.答えは' + answer + 'です.');
+        print = '間違い.残念でした.答えは' + answer + 'です.';
     } else if(yoso < answer){
-        console.log('間違い.答えはもっと大きいですよ');
+        print = '間違い.答えはもっと大きいですよ';
     } else if(yoso > answer) {
-        console.log('間違い.答えはもっと小さいですよ');
+        print = '間違い.答えはもっと小さいですよ';
     } 
+    let resultNode = document.querySelector('p#result') ;
+    // let rp = document.createElement('p');
+    // rp.textContent = print;
+    // resultNode.insertAdjacentElement('beforeend',rp);
+    resultNode.textContent = print;
 }
