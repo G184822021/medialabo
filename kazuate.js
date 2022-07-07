22711423649414026369081056a9947651ecdbcb
G184822021 <218482@st.takushoku-u.ac.jp>
2022-07-07 15:21:30 +0900

    2022/07/07　変更　追加　

--- kazuate.js
+++ kazuate.js
@@ -1,34 +1,34 @@
 
 let answer = Math.floor(Math.random()*10) + 1;
-console.log('答え(デバッグ用) ' + answer);
+let isGameEnd = false;
 let answerCount = 0;
+let guess = document.querySelector('#print');
+console.log('答え(デバッグ用) ' + answer);
+guess.addEventListener('click',hantei);
 
-hantei();
-hantei();
-hantei();
-hantei();
 function hantei(){
-    let yoso = 4;
-    answerCount = answerCount + 1;
     let print;
-    let answerNode = document.querySelector('span#answerCount');
+    let answerNode;
+    let inputNode = document.querySelector('input[name="yoso"]');
+    let guess     = parseInt(inputNode.value);
+    answerCount++;
+    answerNode = document.querySelector('span#answerCount');
     answerNode.textContent = answerCount;
     answerNode = document.querySelector('span#yoso');
-    answerNode.textContent = yoso;
-    if(answerCount > 3) {
+    answerNode.textContent = guess;
+    if(isGameEnd) {
         print = '答えは ' + answer + ' でした.すでにゲームは終わっています.';
-    } else if(yoso === answer){
+    } else if(guess === answer){
         print = '正解です.おめでとう！';
+        isGameEnd = true;
     } else if(answerCount === 3){
         print = '間違い.残念でした.答えは' + answer + 'です.';
-    } else if(yoso < answer){
+        isGameEnd = true;
+    } else if(guess < answer){
         print = '間違い.答えはもっと大きいですよ';
-    } else if(yoso > answer) {
+    } else if(guess > answer) {
         print = '間違い.答えはもっと小さいですよ';
     } 
     let resultNode = document.querySelector('p#result') ;
-    // let rp = document.createElement('p');
-    // rp.textContent = print;
-    // resultNode.insertAdjacentElement('beforeend',rp);
     resultNode.textContent = print;
 }
