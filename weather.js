a2e450affb58c56ea470ca1237445fce94c549a7
G184822021 <218482@st.takushoku-u.ac.jp>
2022-07-27 20:32:50 +0900

    2022/07/27　変更

--- weather.js
+++ weather.js
@@ -48,42 +48,227 @@ let data = {
 ////////// 課題3-2 ここからプログラムを書こう
 /* データを入れる処理
 */
+let countryId = [
+360630 ,
+524901 ,
+993800 ,
+1816670 ,
+1850147,
+1880252 ,
+2147714 ,
+2643743 ,
+2968815 ,
+3451189 ,
+5128581 ,
+5368361 ]
+let countryName = [
+'カイロ',
+'モスクワ',
+'ヨハネスブルク',
+'北京',
+'東京',
+'シンガポール',
+'シドニー',
+'ロンドン',
+'パリ',
+'リオデジャネイロ',
+'ニューヨーク',
+'ロサンゼルス'
+]
 let weatherDatas;// = {name : weatherDates.name,temp_max : weatherDates.main.temp_max, temp_min : weatherDates.main.temp_min};
-let rankingDates;
-for(let data in weatherDatas){
-  rankingDates 
-}
+let rankingDates = new Array(12);
+let rank;
+let i1;
+let id = 1850147;
+
 // 
-let inputNode = document.querySelector('input[name="search"]');
-let id = parseInt(inputNode.value);
 
 let b1 = document.querySelector('button');
 b1.addEventListener('click', sendRequest);
+let c1 = document.querySelector('#cell1');
+c1.addEventListener('click', changeColor);
+c1.addEventListener('click', printRnking);
+let c2 = document.querySelector('#cell2');
+c2.addEventListener('click', changeColor);
+c2.addEventListener('click', sendRequest);
+let c3 = document.querySelector('#cell3');
+c3.addEventListener('click', changeColor);
+c3.addEventListener('click', showWord);
+function showWord(event){
+  deletgraph();
+  
+  for(let i = 0; i < countryId.length;i++){
+    let element = document.createElement('td');
+    element.id = countryId[i];
+    element.addEventListener('click', sendRequest1);
+    let place = document.querySelector('tr#country');
+    if(i > countryId.length /2){
+      place = document.querySelector('tr#speed');
+    }
+    element.textContent = countryName[i];
+    place.insertAdjacentElement('beforeend',element);
+  }
+}
+function changeColor(event) {
+	let div = event.target.className;
+	let d = document.querySelectorAll('#main');
+  for(let i of d){
+    i.className = div;
+  }
+}
+function printRnking(event){
+  i1 = 0;
+  for(i1 = 0; i1 < countryId.length;i1++){
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + countryId[i1] + '.json';
+  console.log(i1);
+  axios.get(url)
+  .then(select)
+  .catch(showError)
+  .then(finish);
+  }
 
+}
+function select(resp){
+	weatherDatas = resp.data;
+	if (typeof weatherDatas === 'string') {
+		weatherDatas = JSON.parse(weatherDatas);
+	}
+  setRank(weatherDatas);
+}
+function setRank(weatherDatas){
+  for(let i = 0; i < rankingDates.length;i++){
+    if(rankingDates[i] != null) continue;
+    let data = { temp_max : weatherDatas.main.temp_max,
+      country : weatherDatas.name
+
+    }
+  rankingDates[i] = data;
+  break;
+}
+sort(rankingDates);
+printRanking(rankingDates);
+}
+function sort(rankingDates){
+  for(var i = 0; i<rankingDates.length-1;i++){
+    let temp = rankingDates[i].temp_max;
+    let max = temp
+    let k = i;
+    for(var j = i+ 1; j<rankingDates.length;j++){
+
+        if(max <rankingDates[j].temp_max){
+            max = rankingDates[j].temp_max;
+            k = j;
+        }
+    }
+    var tmp = rankingDates[i];
+    rankingDates[i] = rankingDates[k];
+    rankingDates[k] =tmp;
+
+    console.log(rankingDates);
+  }
+}
+function printRanking(datas){
+  let name = new Array(rankingDates.length);
+  let temp = new Array(rankingDates.length);
+  deletgraph();
+  
+  for(let i = 0; i < rankingDates.length;i++){
+    name[i] = datas[i].country;
+    temp[i] = datas[i].temp_max;
+    console.log(name[i]);
+  }
+    let element = document.createElement('td');
+    let place = document.querySelector('tr#name');
+    element.textContent =  "<ベスト5位>";
+    place.insertAdjacentElement('beforeend',element);
+  for(let i = 0; i < 5;i++){
+    let element = document.createElement('td');
+    let place = document.querySelector('tr#country');
+    element.textContent = i + 1 + "位";
+    place.insertAdjacentElement('beforeend',element);
+  }
+  for(let i = 0; i < 5;i++){
+    let element = document.createElement('td');
+    let place = document.querySelector('tr#weather');
+    element.textContent = name[i];
+    place.insertAdjacentElement('beforeend',element);
+  }
+  for(let i = 0; i < 5;i++){
+    let element = document.createElement('td');
+    let place = document.querySelector('tr#temp_max');
+    element.textContent = temp[i] + "℃";
+    place.insertAdjacentElement('beforeend',element);
+  }
+  
+}
 function printDate(weatherDatas){
- let  datas = {name : weatherDatas.name,
+  let  datas = {name : weatherDatas.name,
     temp_max : weatherDatas.main.temp_max, 
     temp_min : weatherDatas.main.temp_min,
     country : weatherDatas.sys.country,
-    weather : weatherDatas.weather.description,
+    weather : weatherDatas.weather[0].description,
     speed : weatherDatas.wind.speed
   }
-  for(let data in datas){
-      let element = document.createElement('td');
-      let place = document.querySelector('tr#'+ data);
-      console.log(data[weather]);
-      element.textContent = datas[data];
-      place.insertAdjacentElement('beforeend',element);
+  let label = {name: "都市",
+  temp_max:' 最高気温   ',
+  temp_min:' 最低気温         ',
+  country :' 国   ',
+  weather :' 天気   ',
+  speed :  ' 風速   '
+ }
+ deletgraph();
+  for(let data in label){
+    let element = document.createElement('td');
+    let place = document.querySelector('tr#'+ data);
+    element.textContent = label[data];
+    place.insertAdjacentElement('beforeend',element);
   }
-    
-      
+  for(let data in datas){
+    let element = document.createElement('td');
+    let place = document.querySelector('tr#'+ data);
+    console.log(data[weather]);
+    element.textContent = datas[data];
+    place.insertAdjacentElement('beforeend',element);
+  }
+  let d = document.querySelectorAll('#main');
+  for(let i of d){
+    i.className = "red";
+  }
+  
+}
+
+function deletgraph() {
+  let all = document.querySelectorAll('td');
+  for (let data of all) {
+    data.remove();
+  }
 }
 
 // 通信を開始する処理
 function sendRequest() {
-	// URL を設定
+  // URL を設定
+  let inputNode = document.querySelector('input[name="search"]');
+  for(let i = 0; i < countryName.length;i++){
+    if(inputNode.value === countryName[i]){
+     id = countryId[i];
+    }
+  }
 	let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id + '.json';
-
+  console.log(inputNode);
+  console.log(id);
+  
+	// 通信開始
+	axios.get(url)
+		.then(showResult)
+		.catch(showError)
+		.then(finish);
+}
+function sendRequest1(event) {
+  // URL を設定
+   id = event.target.id;
+	let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id + '.json';
+  console.log(id);
+  
 	// 通信開始
 	axios.get(url)
 		.then(showResult)
@@ -97,7 +282,7 @@ function showResult(resp) {
 	weatherDatas = resp.data;
 
 	// data が文字列型なら，オブジェクトに変換する
-	if (typeof weatherDates === 'string') {
+	if (typeof weatherDatas === 'string') {
 		weatherDatas = JSON.parse(weatherDatas);
 	}
 
